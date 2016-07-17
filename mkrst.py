653eeb67d417b647c5250df97d07ca8092999f34
Kazuhiro Misumi <misumi@inx1.icex-local.abiko.denken.or.jp>
2016-07-17 16:29:21 +0900

    Bug fix regarding NTIMES and DSTART.

--- mkrst.py
+++ mkrst.py
@@ -17,7 +17,6 @@ def mod_file(line,file):
 
 def mod_ntimes(line,val):
   x = line.split("==")
-  x[1] = int(x[1]) + int(val)
   x[1] = " "+str(x[1])
   line = "==".join(x)
   line = line + "\n"
@@ -30,13 +29,23 @@ def mod_dt(line,val):
   line = line + "\n"
   return line
 
+def mod_dstart(line,val):
+  x = line.split("=")
+  y = x[1].split("!")
+  x[1] = float(y[0]) + float(val)
+  x[1] = " "+str(x[1])+" ! "+y[1]
+  line = "=".join(x)
+  return line
+
 case_name = os.environ['CASE_NAME']
 r_num = os.environ['R_NUM']
 p_num = int(r_num)-1
 p_num = str(p_num)
 p_num = p_num.zfill(3)
-d_ntimes = os.environ['D_NTIMES']
+ntimes = os.environ['NTIMES']
 dt = os.environ['DT']
+d_dstart = os.environ['D_DSTART']
+
 
 inifile = os.environ['WORK_DIR'] + "/out/" + case_name + ".r." + p_num + ".nc"
 rstfile = os.environ['WORK_DIR'] + "/out/" + case_name + ".r." + r_num + ".nc"
@@ -67,9 +76,11 @@ for line in fi0:
     elif re.match(r'^\s*AVGNAME\s*==',line):
       line = mod_file(line, avgfile)
     elif re.match(r'^\s*NTIMES\s*==',line):
-      line = mod_ntimes(line, d_ntimes)
+      line = mod_ntimes(line, ntimes)
     elif re.match(r'^\s*DT\s*==',line):
       line = mod_dt(line, dt)
+    elif re.match(r'^\s*DSTART\s*=',line):
+      line = mod_dstart(line, d_dstart)
   fo0.write(line)
   
 fi0.close()
